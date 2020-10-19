4d0e7ea444e8c389007f38b105879142ec8d7375
ariandy <ariandyy@gmail.com>
2020-10-19 22:53:15 +0200

    add byobu tmux fix for vim C-Left and C-Right

--- init.sh
+++ init.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+DOTFILES_DIR=$(pwd)
+echo "==> Working dir: ${DOTFILES_DIR}"
 cd ~
 
 echo "==> Global gitignore"
-ln -s .dotfiles/gitignore_global .gitignore_global
+ln -s ${DOTFILES_DIR}/gitignore_global .gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 
 echo "==> vimrc"
-ln -s .dotfiles/vimrc .vimrc
+ln -s ${DOTFILES_DIR}/vimrc .vimrc
+
+echo "==> byobu tmux fixes"
+mkdir -p ~/.byobu
+if [ -f ~/.byobu/.tmux.conf ]; then
+    cat ${DOTFILES_DIR}/byobu/.tmux.conf >> ~/.byobu/.tmux.conf
+fi
 
 echo "==> Done!"
