2445ce84f52033d5485564cd8e72aa923f79d64e
ariandy <ariandyy@gmail.com>
2021-02-10 16:19:18 +0100

    set verbose for the commands

--- init.sh
+++ init.sh
@@ -5,17 +5,17 @@ echo "==> Working dir: ${DOTFILES_DIR}"
 cd ~
 
 echo "==> Global gitignore"
-ln -s ${DOTFILES_DIR}/gitignore_global .gitignore_global
+ln -sv ${DOTFILES_DIR}/gitignore_global .gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
 
-echo "==> vimrc"
-ln -s ${DOTFILES_DIR}/vimrc .vimrc
+echo "==> vim"
+ln -sv ${DOTFILES_DIR}/vimrc .vimrc
 mkdir -pv ~/.vim/colors
 cp -v ${DOTFILES_DIR}/.vim/colors/solarized.vim ~/.vim/colors/
 
 echo "==> byobu tmux fixes"
 echo "==> TODO: if file empty/nonexistent, do symlink instead"
-mkdir -p ~/.byobu
+mkdir -pv ~/.byobu
 if [ -f ~/.byobu/.tmux.conf ]; then
     cat ${DOTFILES_DIR}/byobu/.tmux.conf >> ~/.byobu/.tmux.conf
 fi
