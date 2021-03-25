75d4a275c275e313830c806fcc9a1c54f024f530
ariandy <ariandyy@gmail.com>
2021-03-25 16:09:39 +0100

    add checking if file is not empty for byobu

--- init.sh
+++ init.sh
@@ -14,7 +14,8 @@ ln -sv ${DOTFILES_DIR}/vimrc .vimrc
 echo "==> byobu tmux fixes"
 echo "==> TODO: if file empty/nonexistent, do symlink instead"
 mkdir -pv ~/.byobu
-if [ -f ~/.byobu/.tmux.conf ]; then
+if [ -s ~/.byobu/.tmux.conf ]; then
+    echo "appending custom tmux conf into ~/.byobu/.tmux.conf"
     cat ${DOTFILES_DIR}/byobu/.tmux.conf >> ~/.byobu/.tmux.conf
 fi
 
